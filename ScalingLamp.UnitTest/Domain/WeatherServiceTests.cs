d41e73f00bc860af3baa7d07efa7187c60df1478
Teng Wei Song <weisong.teng.work@gmail.com>
2023-09-17 23:23:46 +0800

    add unit test for domain services

---
+++ ScalingLamp.UnitTest/Domain/WeatherServiceTests.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeItEasy;
+using ScalingLamp.Domain.Models.DAOs;
+using ScalingLamp.Domain.Models.DomainModels;
+using ScalingLamp.Domain.Persistence;
+using ScalingLamp.Domain.Services;
+using Xunit;
+
+namespace ScalingLamp.UnitTest.Domain
+{
+    public class WeatherServiceTests
+    {
+        private readonly ICityRepository _cityRepository;
+        private readonly IVariableRepository _variableRepository;
+
+        private readonly List<VariableDao> _variables;
+        private readonly HottestCityDao _hottestCityDao;
+        private readonly MoistestCityDao _moistestCityDao;
+
+        public WeatherServiceTests()
+        {
+            _cityRepository = A.Fake<ICityRepository>();
+            _variableRepository = A.Fake<IVariableRepository>();
+
+            _variables = new List<VariableDao>
+            {
+                new VariableDao(new Variable{
+                    Name = "Temperature",
+                    Unit = "C",
+                    Value = "100",
+                    City = new City
+                    {
+                        CityName = "My City"
+                    }
+                })
+            };
+
+            A.CallTo(() => _variableRepository.GetVariablesAsync(
+                A<string>.Ignored,
+                A<DateTimeOffset>.Ignored,
+                A<DateTimeOffset>.Ignored,
+                A<string>.Ignored))
+                .Returns(_variables);
+            
+            _hottestCityDao = new HottestCityDao
+            {
+                CityName = "My Hottest City",
+                Count = 10
+            };
+            A.CallTo(() => _cityRepository.GetHottestCityAsync())
+                .Returns(_hottestCityDao);
+
+            _moistestCityDao = new MoistestCityDao
+            {
+                CityName = "My Hottest City",
+                Average = 10.6
+            };
+            A.CallTo(() => _cityRepository.GetMoistestCityAsync())
+                .Returns(_moistestCityDao);
+        }
+
+        [Fact]
+        public async Task GetVariablesAsync_GivenValidInput_CompletesSuccessfully()
+        {
+            var cityName = "My City";
+            var variableName = "Temperature";
+            var startDate = DateTimeOffset.Parse("2023-01-01");
+            var endDate = DateTimeOffset.Parse("2023-01-31");
+
+            var service = new WeatherService(_cityRepository, _variableRepository);
+            var result = service.GetVariablesAsync(variableName, startDate, endDate, cityName);
+
+            Assert.True(result.IsCompletedSuccessfully);
+            Assert.Equal(_variables.Count, (await result).Count);
+        }
+
+        [Fact]
+        public async Task GetHottestCityAsync_CompletesSuccessfully()
+        {
+            var service = new WeatherService(_cityRepository, _variableRepository);
+            var result = service.GetHottestCityAsync();
+
+            Assert.True(result.IsCompletedSuccessfully);
+            Assert.Equal(_hottestCityDao, await result);
+        }
+
+        [Fact]
+        public async Task GetMoistestCityAsync_CompletesSuccessfully()
+        {
+            var service = new WeatherService(_cityRepository, _variableRepository);
+            var result = service.GetMoistestCityAsync();
+
+            Assert.True(result.IsCompletedSuccessfully);
+            Assert.Equal(_moistestCityDao, await result);
+        }
+    }
+}
